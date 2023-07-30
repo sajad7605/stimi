da505614cd29ca75c9d503ce28729738a17203b4
sajad <s.radvar@gmai.com>
2023-07-30 21:48:10 +0330

    fix file locations

--- src/reports-php/StiJavaScript.php
+++ src/reports-php/StiJavaScript.php
@@ -45,7 +45,7 @@ class StiJavaScript
         $result = '';
         foreach ($scripts as $name) {
             $product = strpos($name, 'dashboards') > 0 ? 'dashboards-php' : 'reports-php';
-            $result .= "<script src=\"/vendor/sajad7605/$product/public/scripts/$name\" type=\"text/javascript\"></script>\n";
+            $result .= "<script src=\"/vendor/sajad7605/stimi/$product/public/scripts/$name\" type=\"text/javascript\"></script>\n";
         }
 
         return $result;
--- src/reports-php/laravel/StiServiceProvider.php
+++ src/reports-php/laravel/StiServiceProvider.php
@@ -4,13 +4,13 @@ namespace Stimulsoft\Laravel;
 
 if (class_exists('\Illuminate\Support\ServiceProvider'))
 {
-	class StiServiceProvider extends \Illuminate\Support\ServiceProvider
-	{
-		public function boot()
-		{
-			\Illuminate\Support\Facades\Route::get('/vendor/sajad7605/stimi/reports-php/public/scripts/{file}', function ($file) {
-				return file_get_contents(__DIR__ . "/../../report/public/scripts/$file");
-			});
-		}
-	}
+    class StiServiceProvider extends \Illuminate\Support\ServiceProvider
+    {
+        public function boot()
+        {
+            \Illuminate\Support\Facades\Route::get('/vendor/sajad7605/stimi/reports-php/public/scripts/{file}', function ($file) {
+                return file_get_contents(__DIR__ . "/../../../public/scripts/$file");
+            });
+        }
+    }
 }
