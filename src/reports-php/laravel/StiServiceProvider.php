ce8cc7cd76c271a57b6dd0493ca3298af3943009
sajad <s.radvar@gmai.com>
2023-07-30 21:43:35 +0330

    change report-php

--- src/reports-php/laravel/StiServiceProvider.php
+++ src/reports-php/laravel/StiServiceProvider.php
@@ -8,8 +8,8 @@ if (class_exists('\Illuminate\Support\ServiceProvider'))
 	{
 		public function boot()
 		{
-			\Illuminate\Support\Facades\Route::get('/vendor/sajad7605/stimi/public/scripts/{file}', function ($file) {
-				return file_get_contents(__DIR__ . "/../../public/scripts/$file");
+			\Illuminate\Support\Facades\Route::get('/vendor/sajad7605/stimi/reports-php/public/scripts/{file}', function ($file) {
+				return file_get_contents(__DIR__ . "/../../report/public/scripts/$file");
 			});
 		}
 	}
